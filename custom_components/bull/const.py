bbb62c9fdfd4acfe8088cdb6b699d7994ad47f34
Mimi <1119186082@qq.com>
2025-01-02 17:27:09 +0800

    Support new globalProductId 180

--- custom_components/bull/const.py
+++ custom_components/bull/const.py
@@ -7,5 +7,5 @@ BULL_DEVICES = "bull_devices"
 APPSECRET = b"t3f9hqri8ciuici50aem25xmcyqsopey"
 API_URL = "https://api.iotbull.com"
 
-SWITCH_PRODUCT_ID = [4, 5, 6, 7, 13, 14, 34, 35, 36]
+SWITCH_PRODUCT_ID = [4, 5, 6, 7, 13, 14, 34, 35, 36, 180]
 COVER_PRODUCT_ID = [31]
