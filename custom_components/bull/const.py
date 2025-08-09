b0203eaf2dd04076f7b1e7ca1ef07ff5479cfa26
Mimi <1119186082@qq.com>
2025-08-10 00:53:57 +0800

    Support device 102 and 104

--- custom_components/bull/const.py
+++ custom_components/bull/const.py
@@ -10,7 +10,7 @@ SUPPORTED_PLATFORMS = ["switch", "sensor", "cover"]
 APPSECRET = b"t3f9hqri8ciuici50aem25xmcyqsopey"
 API_URL = "https://api.iotbull.com"
 
-SWITCH_PRODUCT_ID = {4, 5, 6, 7, 13, 14, 30, 34, 35, 36, 53, 149, 157, 158, 159, 180}
+SWITCH_PRODUCT_ID = {4, 5, 6, 7, 13, 14, 30, 34, 35, 36, 53, 102, 104, 149, 157, 158, 159, 180}
 CHARGER_PRODUCT_ID = {75, 196}
 COVER_PRODUCT_ID = {31, 56}
 
