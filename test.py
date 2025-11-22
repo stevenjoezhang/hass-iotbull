4967dda810b097d6b1a543de0b6e93ee266a259d
Mimi <1119186082@qq.com>
2025-11-22 16:25:24 +0800

    Update test script

--- test.py
+++ test.py
@@ -1,31 +1,38 @@
 import asyncio
 import logging
-from http.client import HTTPConnection
-
-putrequest = HTTPConnection.putrequest
-# https://github.com/home-assistant/core/blob/2023.7.2/homeassistant/block_async_io.py
+import dotenv
+import os
 from custom_components.bull.api import BullApi
-
-HTTPConnection.putrequest = putrequest
+from homeassistant.util.hass_dict import HassDict
+from homeassistant.components.network import Network
+from homeassistant.core_config import Config
+from homeassistant.core import EventBus
 
 logging.basicConfig(level=logging.DEBUG)
+dotenv.load_dotenv()
 
 
 class FakeHass:
-    async def async_add_executor_job(self, cb):
+    def __init__(self):
+        self.data = HassDict()
+        self.config = Config(self, os.path.dirname(__file__))
+        self.bus = EventBus(self)
+        self.data["network"] = Network(self)
+
+    async def async_create_task(self, cb):
         return cb()
 
 
 async def test():
-    username = input("请输入您的用户名: ")
-    password = input("请输入您的密码: ")
+    username = os.getenv("BULL_USERNAME") or input("请输入您的用户名: ")
+    password = os.getenv("BULL_PASSWORD") or input("请输入您的密码: ")
     bull_api = BullApi(FakeHass())
     await bull_api.async_login_mos(username, password)
     await bull_api.async_get_families()
     for family in bull_api.families:
         print(family)
         await bull_api.async_switch_family(family["familyId"])
-        await bull_api.async_get_devices_list()
+        await bull_api.async_get_rooms_mos()
     print(bull_api.device_list)
 
 
